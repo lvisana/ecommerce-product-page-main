ba52078309c2016b47e7afc29359637fe27d063c
lvisana <90291636+lvisana@users.noreply.github.com>
2022-02-22 00:21:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -24,13 +24,13 @@ function sliderImages() {
         let currentImg
         if (this.classList.contains('btn-next')) {
             currentImg = element.getAttribute('src')
-            if (currentImg !== '/images/image-product-4.jpg') {
+            if (currentImg !== './images/image-product-4.JPG') {
             let nextImg = currentImg.replace(/\d/, function(n){ return ++n })
             element.src = nextImg
             }
         } else {
         currentImg = element.getAttribute('src')
-        if (currentImg !== './images/image-product-1.jpg') {
+        if (currentImg !== './images/image-product-1.JPG') {
         let nextImg = currentImg.replace(/\d/, function(n){ return --n })
         element.src = nextImg
         }
